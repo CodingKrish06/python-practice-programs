733c513a36743636ec458f0f0bde0fcfbb271367
CodingKrish06 <venishanmugam08@gmail.com>
2026-06-06 11:37:35 +0530

    Updated files

--- Dictionaries/String.py
+++ Dictionaries/String.py
@@ -29,3 +29,43 @@ for i in str:
         res += chr(ord(i)-32)
 print(res)
 
+# 6)
+
+str  = input()
+arr = str.split()
+print(arr)
+print(" ".join(arr))
+
+# 7)Replace the one word to anthon word
+
+str = input()
+str = str.replace("l","A")
+print(str)
+
+# 8)Program reservee without using inbulid function
+
+str = input()
+arr = list(str)
+print(arr)
+
+start = 0 
+end = len(str)-1
+
+while start < end:
+    arr[start],arr[end] = arr[end],arr[start]
+    start += 1
+    end -= 1
+rev = "".join(arr)
+print(str == rev)
+
+# 9)
+
+str = input()
+ch = input()[0]
+first = last =-1
+for i in range(len(str)):
+    if str[i] == ch:
+        if first == -1:
+            first = i
+        last = i
+print(first,last)
